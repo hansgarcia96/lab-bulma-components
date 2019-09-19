8809a76ab42b61d67e2b893f5bd0dbd7fad64b3e
Hans Garcia <hansgarcia96@gmail.com>
2019-09-18 22:39:46 -0400

    nav and forms done

---
+++ bulma/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Nav from './components/Nav'
+import FormField from './components/FormField'
+import CoolButton from './components/CoolButton'
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+
+      <Nav 
+      
+      
+      />
+
+<FormField label="Name" type="text" placeholder="e.g Alex Smith" />
+<FormField label="Email" type="email" placeholder="e.g. alexsmith@gmail.com" />
+
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ bulma/src/components/CoolButton.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export default class CoolButton extends Component {
+    render() {
+        return (
+
+            <button class="button is-rounded my-class is-danger is-small">Button 1</button>
+
+
+        );
+    }
+}
